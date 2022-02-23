cd1cdb795636d852d06f344ac5046948b9bef5e9
Desiree Quade <nekodez@gmail.com>
2022-02-23 15:43:27 -0600

    added migrations

---
+++ Migrations/20220223214137_CreatePetsTable.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace dotnet_bakery.Migrations
+{
+    public partial class CreatePetsTable : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
